8ac4ca1434d56b2523b0ead8d203301b936ecf08
Carl.Edmunds <carledmunds90@googlemail.com>
2018-01-08 12:19:45 +0000

    Adding a new controller
    - Adding a simple "HelloController" that serves the /greeting URL

--- src/main/java/com/pluralsight/eventtracker/WebConfig.java
+++ src/main/java/com/pluralsight/eventtracker/WebConfig.java
@@ -3,6 +3,11 @@ package com.pluralsight.eventtracker;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+/*
+ * The @EnableWebMvc annotation is effectively used in place of the previous servlet-config.xml file.
+ * The bean definitions that were made within there are now all instead created within this WebConfig class.
+ */
+
 @Configuration
 @EnableWebMvc
 public class WebConfig
